57996c37ba67d5578f8daeb55eab481f91e785d6
Xhibitbrown <kusiadubofour72@gmail.com>
2023-01-12 09:04:34 -0800

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    for r in number:
-         if ord(r) >= 98 and ord(r) <= 122:
-              r = chr(ord(r) - 32)
-               print("{}".format(r), end="")
-               print("")
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
